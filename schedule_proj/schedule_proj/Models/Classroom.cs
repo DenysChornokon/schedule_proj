fd3a0f2697ac7ff694ec8203ac741c85619c5484
Denys Chornokon <denyschornokon3@gmail.com>
2025-02-09 20:07:36 +0200

    #2

---
+++ schedule_proj/schedule_proj/Models/Classroom.cs
@@ -0,0 +1,8 @@
+namespace schedule_proj.Models
+
+    public class Classroom
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+}
--- schedule_proj/schedule_proj/obj/Release/net8.0/schedule_proj.AssemblyInfo.cs
+++ schedule_proj/schedule_proj/obj/Release/net8.0/schedule_proj.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("schedule_proj")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556d47e26123c46411bec3b8bf359bbc76850920")]
 [assembly: System.Reflection.AssemblyProductAttribute("schedule_proj")]
