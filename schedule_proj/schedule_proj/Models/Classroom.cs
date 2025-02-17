0c15cd1f2334ba7eba99e5a22978af5f10ed82fd
paxawok <yurcherevach@gmail.com>
2025-02-17 13:24:30 +0200

    Fixed formatting

--- schedule_proj/schedule_proj/Models/Classroom.cs
+++ schedule_proj/schedule_proj/Models/Classroom.cs
@@ -1,8 +1,10 @@
 namespace schedule_proj.Models
-
-    public class Classroom
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Capacity { get; set; }
+    public class Classroom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
 }
+
