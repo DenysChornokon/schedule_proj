427c3feff1080f1ce634b3f670ba9867cf77b95f
paxawok <yurcherevach@gmail.com>
2025-02-21 04:34:58 +0200

    #3 added new entities

---
+++ schedule_proj/schedule_proj/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿namespace schedule_proj.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; } 
+        public int GroupId { get; set; } 
+        public int TeacherId { get; set; } 
+        public int CourseId { get; set; } 
+        public int ClassroomId { get; set; } 
+        public DateTime StartTime { get; set; } 
+        public DateTime EndTime { get; set; } 
+        public DayOfWeek DayOfWeek { get; set; } 
+    }
+}
---
+++ schedule_proj/schedule_proj/Models/TimeSlot.cs
@@ -0,0 +1,10 @@
+﻿namespace schedule_proj.Models
+{
+    public class TimeSlot
+    {
+        public int Id { get; set; } 
+        public DateTime StartTime { get; set; } 
+        public DateTime EndTime { get; set; } 
+        public DayOfWeek DayOfWeek { get; set; } 
+    }
+}
